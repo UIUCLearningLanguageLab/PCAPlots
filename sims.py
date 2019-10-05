d4b98c054024ce9d3c1469f6a6d7626df6dc2da5
Philip Huebner <info@philhuebner.com>
2019-10-05 10:38:31 -0500

    Update sims.py

--- sims.py
+++ sims.py
@@ -65,3 +65,84 @@
         plt.tight_layout()
         print('{} completed in {:.1f} secs'.format(sys._getframe().f_code.co_name, time.time() - start))
         return fig
+
+    def make_sim_simmat_fig(include_sg=False, vmin=0.9, config_id=1):
+        """
+        Returns fig showing similarity matrix of probe similarity matrices of multiple models
+        """
+        start = time.time()
+        # init term_simmat_mat
+        if include_sg:
+            sg_types = list(np.load(os.path.join(GlobalConfigs.SG_DIR, 'sg_types.npy')).tolist())
+            terms = list(set(model_groups[0][0].hub.train_terms.types) & set(sg_types))
+            dim1 = model_groups[0][0].hub.probe_store.num_probes * len(terms)
+        else:
+            sg_types = None
+            terms = model_groups[0][0].hub.train_terms.types
+            dim1 = model_groups[0][0].hub.probe_store.num_probes * len(terms)
+        num_models = len([model for models in model_groups for model in models])
+        term_simmat_mat = np.zeros((num_models, dim1))
+        # load data
+        row_ids = iter(range(num_models))
+        probe_term_ids = [model_groups[0][0].hub.train_terms.types.index(probe_)
+                          for probe_ in model_groups[0][0].hub.probe_store.types]
+        term_term_ids = [model_groups[0][0].hub.train_terms.types.index(term)
+                         for term in terms]
+        group_names = []
+        for model_desc, models in zip(model_descs, model_groups):
+            for model in models:
+                group_name = model_desc.split('\n')[config_id].split('=')[-1]
+                group_names.append(group_name)
+                probe_term_simmat = model.term_simmat[probe_term_ids, :]
+                flattened = probe_term_simmat[:, term_term_ids, ].flatten()
+                term_simmat_mat[next(row_ids), :] = flattened
+        # sim_simmat
+        if include_sg:  # TODO fix
+            sg_probe_term_ids = [sg_types.index(probe) for probe in model_groups[0][0].hub.probe_store.types]
+            sg_term_term_ids = [sg_types.index(term) for term in terms]
+            sg_token_simmat_filenames = sorted([f for f in os.listdir(GlobalConfigs.SG_DIR)
+                                                if f.startswith('sg_token_simmat')])
+            num_sgs = len(sg_token_simmat_filenames)
+            sg_term_simmats_mat = np.zeros((num_sgs, dim1))
+            for model_id, f in enumerate(sg_token_simmat_filenames):
+                sg_probe_term_simmat = np.load(os.path.join(GlobalConfigs.SG_DIR, f))[sg_probe_term_ids, :]
+                flattened = sg_probe_term_simmat[:, sg_term_term_ids].flatten()
+                sg_term_simmats_mat[model_id, :] = flattened
+            group_names += ['skip-gram'] * num_sgs
+            term_simmat_mat = np.vstack((term_simmat_mat, sg_term_simmats_mat))
+        else:
+            term_simmat_mat = term_simmat_mat
+        sim_simmat = pd.DataFrame(term_simmat_mat).T.corr().values
+        # fig
+        fig, ax = plt.subplots(figsize=(FigsConfigs.MAX_FIG_WIDTH, FigsConfigs.MAX_FIG_WIDTH))
+        mask = np.zeros_like(sim_simmat, dtype=np.bool)
+        mask[np.triu_indices_from(mask, 1)] = True
+        sns.heatmap(sim_simmat, ax=ax, square=True, annot=False,
+                    annot_kws={"size": 5}, cbar_kws={"shrink": .5},
+                    vmin=vmin, vmax=1.0, cmap='jet')  # , mask=mask
+        # colorbar
+        cbar = ax.collections[0].colorbar
+        cbar.set_ticks([vmin, 1.0])
+        cbar.set_ticklabels([str(vmin), '1.0'])
+        cbar.set_label('Similarity between Semantic Spaces')
+        # ax (needs to be below plot for axes to be labeled)
+        num_group_names = len(group_names)
+        ax.set_yticks(np.arange(num_group_names) + 0.5)
+        ax.set_xticks(np.arange(num_group_names) + 0.5)
+        ax.set_yticklabels(group_names, rotation=0)
+        ax.set_xticklabels(group_names, rotation=90)
+        plt.tight_layout()
+        print('{} completed in {:.1f} secs'.format(sys._getframe().f_code.co_name, time.time() - start))
+        # export to csv for stats  # TODO put this somewhere else
+        if AppConfigs.SAVE_SIM_SIMMAT:
+            col1 = ['0'] * num_models1 * num_models + ['1'] * num_models2 * num_models
+            col2 = (['0'] * num_models1 + ['1'] * num_models2) * num_models
+            sim_simmat[np.tril_indices(sim_simmat.shape[0], 0)] = np.nan
+            col3 = sim_simmat.flatten()
+            sim_simmat_df = pd.DataFrame(data={'model1': col1,
+                                               'model2': col2,
+                                               'sim': col3})
+            sim_simmat_df = sim_simmat_df.dropna()
+            sim_simmat_df.to_csv(os.path.join(os.path.expanduser("~"), 'sim_simmat_df.csv'), index=False)
+            print('Saved sim_simmat_df.csv to {}'.format(dir))
+        return fig
