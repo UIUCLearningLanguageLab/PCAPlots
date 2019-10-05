ed15075f502a1084696883f34adf041ec78dd252
Philip Huebner <info@philhuebner.com>
2019-10-05 10:07:37 -0500

    Update hierarchical_clustering.py

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -38,3 +38,31 @@
 
     figs = [make_cat_cluster_fig(cat) for cat in model.hub.probe_store.cats]
     return figs
+
+
+def make_multi_cat_clust_fig(cats, metric='cosine'):  # TODO make into config
+    """
+    Returns fig showing hierarchical clustering of probes from multiple categories
+    """
+    start = time.time()
+    # load data
+    df = pd.DataFrame(pd.concat((model.get_single_cat_probe_prototype_acts_df(cat) for cat in cats), axis=0))
+    cat_acts_mat = df.values
+    cats_probe_list = df.index
+    # fig
+    rcParams['lines.linewidth'] = 2.0
+    fig, ax = plt.subplots(figsize=(FigsConfigs.MAX_FIG_WIDTH, 5 * len(cats)), dpi=FigsConfigs.DPI)
+    # dendrogram
+    dist_matrix = pdist(cat_acts_mat, metric)
+    linkages = linkage(dist_matrix, method='complete')
+    dendrogram(linkages,
+               ax=ax,
+               labels=cats_probe_list,
+               orientation='right',
+               leaf_font_size=10)
+    ax.tick_params(axis='both', which='both', top='off', right='off', left='off')
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    print('{} completed in {:.1f} secs'.format(sys._getframe().f_code.co_name, time.time() - start))
+    return fig
