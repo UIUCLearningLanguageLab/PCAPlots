285c996970e33a1018d0fbf0ce17e32dab050db3
phueb <info@philhuebner.com>
2020-02-16 11:25:26 -0600

    remove outdated legend functions

--- src/utils.py
+++ src/utils.py
@@ -42,28 +42,3 @@ def plot_best_fit_line(ax, x, y, fontsize, color='red', zorder=3, x_pos=0.05, y_
     if plot_p:
         p = np.round(linregress(x, y)[3], decimals=8)
         ax.text(x_pos, y_pos - 0.05, 'p = {}'.format(p), transform=ax.transAxes, fontsize=fontsize - 2)
-
-
-def add_double_legend(ax, lines_list, labels, model_descs, y_offset=-0.2):  # requires figure height = 6
-    box = ax.get_position()
-    num_model_groups = len(model_descs)
-    shrink_prop = 0.1 * num_model_groups  # TODO this doesn't always work well
-    ax.set_position([box.x0, box.y0 + box.height * shrink_prop,  # Shrink vertically to make room for legend
-                     box.width, box.height * (1 - shrink_prop)])
-    leg1 = plt.legend([l[0] for l in lines_list], model_descs, loc='upper center',
-                      bbox_to_anchor=(0.5, y_offset), ncol=2, frameon=False, fontsize=config.Fig.legend_fontsize)
-    for lines in lines_list:
-        for line in lines:
-            line.set_color('black')
-    plt.legend(lines_list[0], labels, loc='upper center',
-               bbox_to_anchor=(0.5, y_offset + 0.1), ncol=3, frameon=False, fontsize=config.Fig.legend_fontsize)
-    plt.gca().add_artist(leg1)  # order of legend creation matters here
-
-
-def add_single_legend(ax, model_descs, y_offset=-0.25):
-    box = ax.get_position()
-    shrink_prop = 0.1 * len(model_descs)
-    ax.set_position([box.x0, box.y0 + box.height * shrink_prop,  # Shrink vertically to make room for legend
-                     box.width, box.height * (1 - shrink_prop)])
-    plt.legend(loc='center', fontsize=config.Fig.legend_fontsize,
-               bbox_to_anchor=(0.5, y_offset), ncol=2, frameon=False)
